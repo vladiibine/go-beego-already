1a605e5b2003e1bf9292db1c4353d5a0d346eeda
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-10-07 23:36:11 +0300

    WINgit st! first null pointer error

--- src/main.go
+++ src/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	_ "github.com/vladiibine/go-beego-already/src/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func main() {
+    beego.Get("/petre", func(ctx *context.Context){
+            var a string
+            var x *int
+            a = string(*x)
+            ctx.Output.Body([]byte("hello petre!!!"))
+            ctx.Output.Body([]byte(a))
+        })
 	beego.Run()
 }
 
